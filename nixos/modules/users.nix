a635af92743625d2791e4e1a62497bacf82f55b6
LukasEins <88452621+LukasEins@users.noreply.github.com>
2026-01-10 16:30:50 +0100

    Update users.nix

--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -7,6 +7,7 @@
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.lukas = {
+    shell = pkgs.zsh;
     isNormalUser = true;
     description = "lukas";
     extraGroups = [ "networkmanager" "wheel" ];

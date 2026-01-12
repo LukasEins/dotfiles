4ec413fcd0fe068a142449cdf0e538e7ecbe48a3
LukasEins <88452621+LukasEins@users.noreply.github.com>
2026-01-12 14:03:04 +0100

    Add files via upload

---
+++ nixos/modules/cachyos.nix
@@ -0,0 +1,11 @@
+{ pkgs, inputs, ... }:
+
+{
+  nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.pinned ];
+  boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore;
+
+  nix.settings = {
+    substituters = [ "https://attic.xuyh0120.win/lantian" ];
+    trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
+  };
+}

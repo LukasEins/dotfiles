89363f6a97fc4047f708cf6da60411f1b294c4e0
LukasEins <88452621+LukasEins@users.noreply.github.com>
2026-01-12 14:03:29 +0100

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,43 +1,34 @@
 {
-	description="Lukas NixOS setup";
+  description = "Lukas NixOS setup";
 
-	inputs={
-		nixpkgs.url="github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    # Core
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-		nix-cachyos-kernel.url="github:xddxdd/nix-cachyos-kernel/release";
+    # Kernel & Optimization
+    nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
 
-		quickshell={
-			url="github:outfoxxed/quickshell";
-			inputs.nixpkgs.follows="nixpkgs";
-		};
+    # DE / Shell
+    quickshell = {
+      url = "github:outfoxxed/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-		noctalia={
-			url="github:noctalia-dev/noctalia-shell";
-			inputs.nixpkgs.follows="nixpkgs";
-		};
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-	};
+  outputs = { self, nixpkgs, ... } @ inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
 
+      specialArgs = { inherit inputs; };
 
-	outputs=inputs@{ self, nixpkgs, nix-cachyos-kernel, ... }: {
-		nixosConfigurations.nixos=nixpkgs.lib.nixosSystem {
-
-	specialArgs={ inherit inputs; };
-
-	modules = [
-	./configuration.nix
-	  (
-          { pkgs, ... }:
-          {
-            nixpkgs.overlays = [ nix-cachyos-kernel.overlays.pinned ];
-            boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore;
-            # Binary cache
-            nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
-            nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
-
-            
-          }
-        )	
+      modules = [
+        ./configuration.nix
+        ./modules/cachyos.nix
       ];
     };
   };

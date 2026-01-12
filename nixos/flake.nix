7345ed8d4b68ed5de8d9d21452b9f2c56333a40c
LukasEins <88452621+LukasEins@users.noreply.github.com>
2026-01-12 11:19:34 +0100

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -4,6 +4,8 @@
 	inputs={
 		nixpkgs.url="github:nixos/nixpkgs/nixos-unstable";
 
+		nix-cachyos-kernel.url="github:xddxdd/nix-cachyos-kernel/release";
+
 		quickshell={
 			url="github:outfoxxed/quickshell";
 			inputs.nixpkgs.follows="nixpkgs";
@@ -12,19 +14,29 @@
 		noctalia={
 			url="github:noctalia-dev/noctalia-shell";
 			inputs.nixpkgs.follows="nixpkgs";
-#			inputs.quickshell.follows="quickshell";
 		};
 
 	};
 
 
-	outputs=inputs@{ self, nixpkgs, ... }: {
+	outputs=inputs@{ self, nixpkgs, nix-cachyos-kernel, ... }: {
+		nixosConfigurations.nixos=nixpkgs.lib.nixosSystem {
 
-	nixosConfigurations.nixos=nixpkgs.lib.nixosSystem{
-	specialArgs={ inherit inputs; };		
-	modules=[
-		./configuration.nix
+	specialArgs={ inherit inputs; };
+
+	modules = [
+	./configuration.nix
+	  (
+	  { pkgs, ... }:
+	  {
+	   nixpkgs.overlays = [
+              # Build the kernels on top of nixpkgs version in your flake.
+              # Binary cache may be unavailable for the kernel/nixpkgs version combos.
+              nix-cachyos-kernel.overlays.default
 		
+		];
+		}
+		)
 		];
 	};
 };

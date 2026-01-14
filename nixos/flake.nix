49285ad6aa095b3b1421eb9e35c5dd2b39835e13
LukasEins <88452621+LukasEins@users.noreply.github.com>
2026-01-14 13:16:08 +0100

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -21,7 +21,7 @@
   };
 
   outputs = { self, nixpkgs, hytale-launcher, ... } @ inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.Yuki = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
 
       specialArgs = { inherit inputs; };

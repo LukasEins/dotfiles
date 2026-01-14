aca0657b70689a571e8cf4161f82733d37656e06
LukasEins <88452621+LukasEins@users.noreply.github.com>
2026-01-14 13:15:54 +0100

    Update network.nix

--- nixos/modules/network.nix
+++ nixos/modules/network.nix
@@ -2,7 +2,7 @@
 
 {
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "Yuki"; # Define your hostname.
 # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Enable networking

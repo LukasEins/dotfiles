4b3c75461a6246938017833ea4f9dd2ce9ca3c28
LukasEins <88452621+LukasEins@users.noreply.github.com>
2026-01-20 10:27:16 +0100

    Update audio.nix

--- nixos/modules/audio.nix
+++ nixos/modules/audio.nix
@@ -1,20 +1,20 @@
 { config, pkgs, inputs, ... }:
 
 {
+
   services.pipewire = {
     enable = true; 
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    wireplumber.enable=true;
   };
 
   services.pipewire.extraConfig.pipewire."92-low-latency" = {
     "context.properties" = {
       "default.clock.rate" = 48000;
-      "default.clock.quantum" = 32;
-      "default.clock.min-quantum" = 32;
-      "default.clock.max-quantum" = 32;
+      "default.clock.min-quantum" = 1024;
     };
   };
 

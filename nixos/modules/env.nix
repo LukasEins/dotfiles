b84581b628de4c3256e4a7780ba5b4c1d02ed124
LukasEins <88452621+LukasEins@users.noreply.github.com>
2026-01-20 10:28:19 +0100

    Update env.nix

--- nixos/modules/env.nix
+++ nixos/modules/env.nix
@@ -1,14 +1,8 @@
 { config, pkgs, inputs, ... }:
 
 {
-
-
   environment.variables = {
     RUSTICL_ENABLE = "radeonsi";
   };
-
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
-
-
+  #environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }

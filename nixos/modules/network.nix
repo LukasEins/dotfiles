162be87be891bc6ccb887a9b652c3c8e094fbcda
LukasEins <88452621+LukasEins@users.noreply.github.com>
2026-02-10 15:08:54 +0100

    Update network.nix

--- nixos/modules/network.nix
+++ nixos/modules/network.nix
@@ -3,9 +3,28 @@
 {
 
   networking.hostName = "Yuki"; # Define your hostname.
-# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Enable networking
+/*  networking.nameservers = [
+    "1.1.1.1"
+    "1.0.0.1"
+  ];
+
+    services.resolved = {
+    enable = true;
+    settings.Resolve={
+    DNSSEC = "true";
+    Domains =  "~.";
+    FallbackDns = [
+      "1.1.1.1"
+      "1.0.0.1"
+    ];
+    DNSOverTLS = "true";
+  };
+};*/
+  services.resolved.enable=true;
+  services.cloudflare-warp.enable=true;
+
+  networking.networkmanager.wifi.powersave = false;
   networking.networkmanager.enable = true;
   networking.firewall.enable = true;
 }

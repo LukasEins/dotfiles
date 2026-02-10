defb91d1b971a6b338eca45998d8e934d22d3f7d
LukasEins <88452621+LukasEins@users.noreply.github.com>
2026-02-10 15:10:19 +0100

    Update audio.nix

--- nixos/modules/audio.nix
+++ nixos/modules/audio.nix
@@ -14,7 +14,9 @@
   services.pipewire.extraConfig.pipewire."92-low-latency" = {
     "context.properties" = {
       "default.clock.rate" = 48000;
+      #"default.clock.quantum" = 32;
       "default.clock.min-quantum" = 1024;
+      #"default.clock.max-quantum" = 32;
     };
   };
 
@@ -37,6 +39,8 @@
       }
     ];
   };
+
+
 
   environment.systemPackages = with pkgs; [
  pavucontrol  

1166b78719b621db9c5ab8d8547861fa75220812
LukasEins <88452621+LukasEins@users.noreply.github.com>
2026-04-07 18:21:43 +0200

    Update programs.nix

--- nixos/modules/programs.nix
+++ nixos/modules/programs.nix
@@ -1,7 +1,13 @@
 { config, pkgs, inputs, ... }:
 
 {
-  programs.firefox.enable=true;
+  programs.firefox={
+	enable=true;
+	package = pkgs.firefox-bin;
+	nativeMessagingHosts.packages = [pkgs.pywalfox-native];
+  };
+  programs.seahorse.enable=true;
+  programs.kdeconnect.enable=true;
   programs.dconf.enable=true;
   programs.niri.enable=true;
   programs.thunar.enable=true;
@@ -9,6 +15,8 @@
   programs.thunar.plugins = with pkgs; [
   thunar-archive-plugin # Requires an Archive manager like file-roller, ark, etc
   thunar-volman
+  thunar-shares-plugin
+  thunar-media-tags-plugin
   ];
 programs.xwayland.enable=true;
 xdg.portal = {

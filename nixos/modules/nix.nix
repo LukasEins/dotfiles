7032bd124f203452176a9b7c98e7f85fe25882ae
LukasEins <88452621+LukasEins@users.noreply.github.com>
2026-04-07 18:20:46 +0200

    Update nix.nix

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -2,15 +2,12 @@
 
 
 {
-
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings.auto-optimise-store=true;
   nix.gc={
 	automatic=true;
 	dates="weekly";
 	options="--delete-older-than-10d";
+	persistent=true;
 	};
-
-
-
 }
